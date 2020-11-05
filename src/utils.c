55b39dfbc43ba886f95dd95558dcd077670d4122
Victor Hofstetter <57238498+VicHofs@users.noreply.github.com>
2020-11-05 17:37:47 -0300

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -4,26 +4,12 @@
 #include <conio.h>
 #include <string.h>
 #include "utils.h"
+/* definir o limite de letras por palavra */
+#define MAX_LETRASPORPALAVRA 32
 //vamos colocar todas as utilidades do jogo nesse arquivo
 //lembrar de incluir a prototipagem no header para cada uma
 
-int randint(int lower, int upper) {
-  srand(time(0) + clock());
-  return (rand() % (upper - lower + 1)) + lower;
-}
-
-void tip(char *word, int *guessed, int *revealed) {
-  int iter = strlen(word) / 3;
-  char c;
-  for (int i = 0; i < iter; i++) {
-    do
-      c = word[randint(0, strlen(word) - 1)];
-    while (revealed[c - 65]);
-    guessed[c - 65] = 1;
-    revealed[c - 65] = 1;
-  }
-}
-
+/* funcao que imprime o desenho da forca */
 void hangman(int strikes) {
   char head = strikes == 6 ? '0' : 'O', torso = '|', l_arm = '/', r_arm = '\\', l_leg = '/', r_leg = '\\';
 
@@ -44,33 +30,99 @@ void hangman(int strikes) {
     );
 }
 
-void word_progress(char *word, int *revealed, int *done) {
-  *done = 1;
-  for (int i = 0; i < strlen(word); i++) {
-    if (word[i] < 65 || word[i] > 90) {
-      printf("%c", word[i]);
+/* funcao que retorna o numero de palavras no arquivo de entrada */
+int quantasPalavrasHaNoArquivoDeEntrada(FILE* arquivoEntrada)
+{
+    int contador = 0; 
+    char buffer[MAX_LETRASPORPALAVRA];
+
+    rewind(arquivoEntrada);
+
+    while(!feof(arquivoEntrada))
+    {
+
+        fscanf(arquivoEntrada, "%s", buffer);
+        contador++;
     }
-    else {
-      if (revealed[word[i] - 65]) printf("%c", word[i]);
-      else {
-        printf("_");
-        if (*done) *done = 0;
-      }
-    }
-  }
-  printf("\n");
+
+    return contador;
 }
 
-void test_char(char *var, int *present, int *revealed, int *guessed, int *strikes) {
-  do 
-    *var = getch();
-  while (*var < 65 || (*var > 90 && *var < 97) || *var > 122);
+/* funcao que retorna uma string com as palavras que estavam no arquivo de entrada */
+char** monta_vocabulario(FILE* arquivoEntrada)
+{
+    int numeroDePalavras = quantasPalavrasHaNoArquivoDeEntrada(arquivoEntrada), i = 0;
+    char buffer[MAX_LETRASPORPALAVRA];
+    char** palavras = malloc(sizeof(char*) * numeroDePalavras);
 
-  if (*var >= 97) *var -= 32;
+    rewind(arquivoEntrada);
+    for (i = 0; i < numeroDePalavras; i++)
+    {   
+        fscanf(arquivoEntrada, "%s", buffer);
+        palavras[i] = (char*)malloc(strlen(buffer)+1);
+        strcpy(palavras[i], buffer); 
+    }
+    return palavras;
 
-  if (present[*var - 65]) revealed[*var - 65] = 1;
+}
 
-  else *strikes += 1;
+/* funcao que escolhe uma palavra aleatoria do arquivo de entrada com base na string que contem as palavras
+que estavam nele */
+char* escolheUmaPalavraAleatoria(char** vocabulario, int numeroPalavras)
+{
+    int indexAleatorio = 0;
+    srand(time(NULL));
+    indexAleatorio = rand() % numeroPalavras;
 
-  guessed[*var - 65] = 1;
+    return vocabulario[indexAleatorio];
+}
+
+/* funcao de suporte que calcula o logaritmo na base 2 de n recursivamente */
+unsigned int logaritmoBase2(unsigned int n) 
+{ 
+    return (n > 1) ? 1 + logaritmoBase2(n / 2) : 0; 
+} 
+
+/* funcao que pre-processa a palavra chave para facilitar a validacao da sugestao/entrada do usuario */ 
+int preProcessamentoPalavra(char*palavra, char letraDesejada)
+{
+    int contador = 0, minhaStringBinaria = 0;
+    int letrasFaltantes = strlen(palavra);
+    for (contador = 0; contador < letrasFaltantes; contador++)
+    {
+        if (palavra[contador] == letraDesejada)
+        {
+            minhaStringBinaria = minhaStringBinaria | 1;
+            
+        }
+
+        if (contador != letrasFaltantes -1)
+        {
+            minhaStringBinaria = minhaStringBinaria << 1;
+        }
+    }
+
+    return minhaStringBinaria;
+
+}
+
+/* funcao que retorna quantas vezes certa letra sugerida pelo usuario esta na palavra chave*/
+int displayProcesso(char* palavra, int alfabetoDaPalavra[], char letraInput)
+{
+    int codigoLetra = (int) letraInput - 97;
+    int contador = 0;
+    int n = alfabetoDaPalavra[codigoLetra];
+    unsigned i, j = 31, k = 0; 
+    if(n == 0) return 0;
+    
+    for (i = 1 << 31; i > 0; i = i / 2, j--, k++) 
+    {
+        int x = logaritmoBase2(i) - (j - k);
+        if (n & i)
+        {
+            palavra[x] = letraInput;
+            contador++;
+        }
+    }
+    return contador;
 }
