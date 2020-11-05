4e9af6884c88ae364ff26d37fe249c674506c222
Victor Hofstetter <57238498+VicHofs@users.noreply.github.com>
2020-11-05 17:38:24 -0300

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,14 @@
-//header com as declarações das utilidades do jogo
-int randint(int lower, int upper);
-
-void tip(char *word, int *guessed, int *revealed);
-
+/* funcao que imprime o desenho da forca */
 void hangman(int strikes);
-
-void word_progress(char *word, int *revealed, int *done);
-
-void test_char(char *var, int *present, int *revealed, int *guessed, int *strikes);
+/* funcao que retorna o numero de palavras no arquivo de entrada */
+int quantasPalavrasHaNoArquivoDeEntrada(FILE* arquivoEntrada);
+/* funcao que retorna uma string com as palavras que estavam no arquivo de entrada */
+char** monta_vocabulario(FILE* arquivoEntrada);
+/* funcao que escolhe uma palavra aleatoria do arquivo de entrada com base na string que contem as palavras que estavam nele */
+char* escolheUmaPalavraAleatoria(char** vocabulario, int numeroPalavras);
+/* funcao de suporte que calcula o logaritmo na base 2 de n recursivamente */
+unsigned int logaritmoBase2(unsigned int n);
+/* funcao que pre-processa a palavra chave para facilitar a validacao da sugestao/entrada do usuario */ 
+int preProcessamentoPalavra(char*palavra, char letraDesejada);
+/* funcao que retorna quantas vezes certa letra sugerida pelo usuario esta na palavra chave*/
+int displayProcesso(char* palavra, int alfabetoDaPalavra[], char letraInput);
